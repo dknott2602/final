eb2eec5bb7fd8d2586b0505c4612683e7a9a0753
dknott2602 <dknott2602@gmail.com>
2025-06-24 12:16:23 -0700

    commit message

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fabars } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fabars)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
