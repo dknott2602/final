84f215c248f22541117a952427311e5884ddca86
dknott2602 <dknott2602@gmail.com>
2025-06-27 10:38:41 -0700

    commit

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,36 +1,52 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Movies = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState(""); // Initialize with a default search term
+  const [filter, setFilter] = useState("DEFAULT"); // State for the filter
 
-  async function renderMovies(filter) {
+  async function renderMovies(search = searchTerm, currentFilter = filter) {
     const response = await fetch(
-      `https://www.omdbapi.com/?apikey=8855abea&s=fast`
+      `https://www.omdbapi.com/?apikey=8855abea&s=${search}`
     );
 
     const data = await response.json();
 
     let moviesData = data.Search;
 
-    if (filter === "A_Z") {
-      moviesData.sort((a, b) => a.Title.localeCompare(b.Title));
-    } else if (filter === "Z_A") {
-      moviesData.sort((a, b) => b.Title.localeCompare(a.Title));
-    } else if (filter === "YEAR") {
-      moviesData.sort((a, b) => Number(b.Year) - Number(a.Year));
+    if (moviesData) {
+      if (currentFilter === "A_Z") {
+        moviesData.sort((a, b) => a.Title.localeCompare(b.Title));
+      } else if (currentFilter === "Z_A") {
+        moviesData.sort((a, b) => b.Title.localeCompare(a.Title));
+      } else if (currentFilter === "YEAR") {
+        moviesData.sort((a, b) => Number(b.Year) - Number(a.Year));
+      }
+      setMovies(moviesData);
+    } else {
+      setMovies([]); // Clear movies if no results
     }
-
-    setMovies(moviesData);
   }
 
-  function filterMovies(event) {
-    renderMovies(event.target.value);
+  function handleSearchChange(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  function handleSearchSubmit() {
+    renderMovies(searchTerm, filter);
+  }
+
+  function handleFilterChange(event) {
+    const newFilter = event.target.value;
+    setFilter(newFilter);
+    renderMovies(searchTerm, newFilter);
   }
 
   useEffect(() => {
-    renderMovies();
-  }, []);
+    renderMovies(); // Initial render when component mounts
+  }, []); // Empty dependency array means this runs once on mount
 
   return (
     <div id="movies__body">
@@ -44,8 +60,8 @@ const Movies = () => {
                 </h2>
                 <select
                   id="filter"
-                  defaultValue="DEFAULT"
-                  onChange={(event) => filterMovies(event.target.value)}
+                  value={filter} // Bind select value to filter state
+                  onChange={handleFilterChange}
                 >
                   <option value="DEFAULT" disabled>
                     Sort
@@ -55,22 +71,46 @@ const Movies = () => {
                   <option value="YEAR">Year</option>
                 </select>
               </div>
+              <div className="input__wrap">
+                <input
+                  type="text"
+                  placeholder="Search By Title or Keyword"
+                  value={searchTerm}
+                  onChange={handleSearchChange}
+                  onKeyPress={(event) => {
+                    if (event.key === "Enter") {
+                      handleSearchSubmit();
+                    }
+                  }}
+                />
+                <div className="search__wrapper" onClick={handleSearchSubmit}>
+                  <FontAwesomeIcon icon="magnifying-glass" />
+                </div>
+              </div>
               <div className="movies">
-                {movies.map((movie) => (
-                  <Link style={{ width: "25%" }} key={movie.imdbID} to={`/movies/${movie.imdbID}`}>
-                    <div className="movie">
-                      <figure className="movie__img--wrapper">
-                        <img className="movie__img" src={movie.Poster} alt="" />
-                      </figure>
-                      <div className="movie__title">
-                        {movie.Title}
+                {movies.length > 0 ? (
+                  movies.map((movie) => (
+                    <Link
+                      style={{ width: "25%" }}
+                      key={movie.imdbID}
+                      to={`/movies/${movie.imdbID}`}
+                    >
+                      <div className="movie">
+                        <figure className="movie__img--wrapper">
+                          <img
+                            className="movie__img"
+                            src={movie.Poster}
+                            alt=""
+                          />
+                        </figure>
+                        <div className="movie__title">{movie.Title}</div>
+                        <div className="movie__year">{movie.Year}</div>
                       </div>
-                      <div className="movie__year">
-                        {movie.Year}
-                      </div>
-                    </div>
-                  </Link>
-                ))}
+                    </Link>
+                  ))
+                ) : (
+                  <div className="no-movies">No movies found.</div>
+                )}
               </div>
             </div>
           </div>
